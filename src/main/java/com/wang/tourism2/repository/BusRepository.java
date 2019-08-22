5e724a66949af149c544458cb5b6238cefbd6bcd
王俊超 <https://gitee.com/woaimaomao>
2019-08-22 21:18:26 +0800

    2019/8/22

--- src/main/java/com/wang/tourism2/repository/BusRepository.java
+++ src/main/java/com/wang/tourism2/repository/BusRepository.java
@@ -4,8 +4,13 @@ import com.wang.tourism2.entity.BusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BusRepository extends JpaRepository<BusEntity,Integer> {
+    List<BusEntity> findAll();
     BusEntity findByBid(int bid);
+    BusEntity save(BusEntity busEntity);
+    BusEntity deleteByBid(int bid);
 
 }
--- src/main/java/com/wang/tourism2/service/BusService.java
+++ src/main/java/com/wang/tourism2/service/BusService.java
@@ -3,10 +3,12 @@ package com.wang.tourism2.service;
 
 import com.wang.tourism2.entity.BusEntity;
 import com.wang.tourism2.repository.BusRepository;
+import org.hibernate.query.criteria.internal.BasicPathUsageException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Service
@@ -16,6 +18,16 @@ public class BusService {
 
     public BusEntity findByBid(int bid){
         return busRepository.findByBid(bid);
+    }
+
+    public BusEntity deleteById(int bid){
+        return busRepository.deleteByBid(bid);
+    }
+    public BusEntity save(BusEntity busEntity){
+        return busRepository.save(busEntity);
+    }
+    public List<BusEntity> findAll(){
+        return busRepository.findAll();
     }
 
 }
